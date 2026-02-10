0524d8d085539d1debd9add9564930ca1c2d58fb
Mohammed-FE2025 <mohammed.amin.yasiin@gmail.com>
2026-02-10 03:27:05 +0100

    🚧 ELements Grabbed From  HTML

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
+// @ts-nocheck
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const form = document.querySelector("form");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const descriptionInput = document.getElementById(
+  "transactionDescreption"
+) as HTMLInputElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const amountInput = document.getElementById(
+  "transactionAmount"
+) as HTMLInputElement;
+
+const balanceDisplay = document.getElementById("balanceDisplay")!;
+const incomeDisplay = document.getElementById("incomeDisplay")!;
+const expenseDisplay = document.getElementById("expenseDisplay")!;
+
+const transactionHistory = document.getElementById("transactionHistory")!;
+const transactions = [];
+
